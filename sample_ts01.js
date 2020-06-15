d5855611f4a5193809dd9e8f9c7513fc1f0ed713
peka2 <d9xj@yahoo.co.jp>
2020-06-15 17:50:22 +0900

    add

---
+++ sample_ts01.js
@@ -0,0 +1,6 @@
+"use strict";
+let a = 10;
+let b = 5;
+a = 20;
+let answer = a * b;
+console.log(answer);
---
+++ sample_ts01.ts
@@ -0,0 +1,8 @@
+let a: number = 10;
+let b: number = 5;
+
+a = 20;
+
+let answer: number = a * b;
+
+console.log(answer);
